13560b7b6e250c30128f3cd423feabee436f8df3
Ivan <uvanes21@yandex.ru>
2022-09-14 09:13:15 +0300

    Initial commit

---
+++ src/main/java/backends/testdiplom/TestDiplomApplication.java
@@ -0,0 +1,13 @@
+package backends.testdiplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDiplomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDiplomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backends/testdiplom/TestDiplomApplicationTests.java
@@ -0,0 +1,13 @@
+package backends.testdiplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDiplomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
